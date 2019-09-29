5d7c6456b3180b96aa48f90d8c6bb13c61e0b89a
Dctaylor <dct828@gmail.com>
2019-09-29 13:44:47 -0400

    Bot V1.1: Bot now logs out of discord and kills its process after running, in order to work with a task scheduler.  Bot will now be run by task scheduler running a .bat file at 6:59 pm PST

--- bot.js
+++ bot.js
@@ -33,22 +33,31 @@ const r = new snoowrap({
 
   
 //Get posts from subreddit and posts it to the correct channel
-new CronJob('00 00 19 * * *', function() {
+new CronJob('00 43 10 * * *', function() {
     try {
         r.getSubreddit('Animemes').getTop({time: 'day'}, {limit: 10}).then(myListing => {
             var index = Math.floor(Math.random() * 10);
             const channel = client.channels.get(config.channelId);
             channel.send(myListing[index].url);
+
+            //listens for messages sent, if it detects the bot has sent its post, it logs the bot out
+            client.on('message', message => {
+                //checks if the message is in the correct channel
+                if(message.channel == channel)
+                    //checks if the author of the message is our bot
+                    if(message.author.id === client.user.id)
+                        //logs the bot out of discord
+                        client.destroy();
+            })
         })
     } catch (error) {
         console.log('There has been a problem with your fetch operation: ', error.message);
     }
 }, null, true, 'America/Los_Angeles');
 
-
+//Lets us know the bot has disconnected and kills the process
+client.on("disconnect", function(event){
+    console.log('Bot disconnecting');
+    process.exit();
+});
     
-
-//post the link
-/*cron.schedule('* 19 * * *', () => {
-    client.channels.get(config.channelId).send('My Message');
-});*/
